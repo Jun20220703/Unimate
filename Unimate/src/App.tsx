5ec56d1bee3af320fc9493a87b277a040d07f2c6
Jun20220703 <kkjhhyu0405@gmail.com>
2025-10-16 19:29:47 +0800

    Update App.tsx

--- Unimate/src/App.tsx
+++ Unimate/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import HomePage from './components/homePage'
+import SignUp from './components/sign-up'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <SignUp />
     </div>
   )
 }
